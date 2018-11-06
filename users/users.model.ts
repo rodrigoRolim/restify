8f4d21bd2ae627053e28dac08b71593d2d057067
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-11-06 14:12:40 -0200

    user model

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,11 +1,21 @@
 
 const users = [
-    {name: 'rivotril', email: 'rivotril@gmail.com'},
-    {name: 'sertralina', email: 'sertralina@gmail.com'}
+  {id: '1', name: 'rivotril', email: 'rivotril@gmail.com'},
+  {id: '2', name: 'sertralina', email: 'sertralina@gmail.com'}
 ]
 
 export class User {
-    static findAll(): Promise<any[]> {
-        return Promise.resolve(users)
-    }
+  static findAll(): Promise<any[]> {
+    return Promise.resolve(users)
+  }
+  static findById(id: string): Promise<any> {
+    return new Promise(resolve => {
+      const filtered = users.filter(user => user.id === id) 
+      let user = undefined
+      if(filtered.length > 0) {
+        user = filtered[0]
+      }
+      resolve(user)
+    })
+  } 
 }
